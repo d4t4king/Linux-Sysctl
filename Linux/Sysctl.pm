8defabe0e0fa8bb2aa7b95c27b2c2e36e72934fd
dataking <dataking@gmail.com>
2016-08-14 19:07:52 -0700

    Getting started

--- Linux/Sysctl.pm
+++ Linux/Sysctl.pm
@@ -6,6 +6,8 @@ use strict;
 use warnings;
 
 use Exporter;
+use Term::ANSIColor;
+use Data::Dumper;
 
 {
 	$Linux::Sysctl::VERSION = '0.0.1';
@@ -13,3 +15,23 @@ use Exporter;
 
 @ISA	= qw/ Exporter /;
 
+sub new {
+	# populate stuff here
+	opendir PROCSYS, "/proc/sys" or die colored("There was a problem reading the /proc/sys directory: $! \n", "bold red");
+	closedir PROCSYS or die colored("There was a problem closing the /proc/sys directory: $! \n", "bold red");
+}
+
+sub get {
+	my $self = shift;
+	my $name = shift;
+
+	return $self->{$name};
+}
+
+sub set {
+	my $self = shift;
+	my $name = shift;
+
+	# write to /proc/sys
+	# return true, if success
+}
